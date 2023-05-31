be93a5300991606c7a7e6beed1a5ca044e02a773
Ananda Tiwari <anandatiwari31@gmail.com>
2023-05-31 23:14:59 +0930

    initial commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+var lisstexample = new List<string>() { "Ananda", "Sapana", "Tiwari", "Neupane" };
+var dic = new Dictionary<string, string>();
+dic["Husband"] = "Ram";
+dic["Wife"] = "Sita";
+string MyName = "Ananda";
+var name1 = "Ananda";
+var name2 = "Tiwari";
+var newname = $"my first and second name are: {name1} {name2}";
+Console.WriteLine(lisstexample[0]);
